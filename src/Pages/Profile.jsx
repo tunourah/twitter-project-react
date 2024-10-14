3009a8593babdbbe44f19a4deb7fe07bc3a651f8
noura <noura.tu19@gmail.com>
2024-10-14 13:58:55 +0300

    fix height

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -34,7 +34,7 @@ const Profile = () => {
 
   return (
     <div className="bg-black  ">
-      <div className="container mx-auto h-screen sm:h-full bg-black">
+      <div className=" min-h-screen container mx-auto h-screen sm:h-full bg-black">
         <div className="w-full grid grid-cols-12">
           <div className=" hidden sm:block sm:col-span-3">
             <Left />
